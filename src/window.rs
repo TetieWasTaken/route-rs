0c43590f1572d6bfb041b8b493038ebcd78bf300
TetieWasTaken <tjvssr@gmail.com>
2023-03-25 17:24:07 +0100

    fix: focus on center

--- src/window.rs
+++ src/window.rs
@@ -33,20 +33,21 @@ pub fn init(roads: Vec<Road>, intersections: Vec<Intersection>) {
         if let Some(r) = e.render_args() {
             gl.draw(r.viewport(), |c, gl| {
                 clear([1.0; 4], gl);
+
+                // Scale the shapes by a factor of 10 and translate them to the center of the window
+                let center = c
+                    .transform
+                    .trans(r.window_size[0] / 2.0, r.window_size[1] / 2.0);
+                let scale = center.scale(10.0, 10.0);
+
                 for road in &roads {
-                    line(
-                        [0.0, 0.0, 0.0, 1.0],
-                        1.0,
-                        road.get_points(),
-                        c.transform,
-                        gl,
-                    );
+                    line([0.0, 0.0, 0.0, 1.0], 1.0, road.get_points(), center, gl);
                 }
                 for intersection in &intersections {
                     ellipse(
                         [0.0, 0.0, 0.0, 1.0],
                         [intersection.lon - 2.0, intersection.lat - 2.0, 4.0, 4.0],
-                        c.transform,
+                        scale.trans(intersection.lon, -intersection.lat),
                         gl,
                     );
                 }
