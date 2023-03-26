2a82651f0994e643c3cc93d7e71acf6b9fa8e44e
TetieWasTaken <tjvssr@gmail.com>
2023-03-26 13:05:48 +0200

    feat: add road type colors

--- src/window.rs
+++ src/window.rs
@@ -34,18 +34,26 @@ pub fn init(roads: Vec<Road>, intersections: Vec<Intersection>) {
             gl.draw(r.viewport(), |c, gl| {
                 clear([1.0; 4], gl);
 
-                // Scale the shapes by a factor of 10 and translate them to the center of the window
                 let center = c
                     .transform
                     .trans(r.window_size[0] / 2.0, r.window_size[1] / 2.0);
                 let scale = center.scale(10.0, 10.0);
 
                 for road in &roads {
-                    line([0.0, 0.0, 0.0, 1.0], 1.0, road.get_points(), center, gl);
+                    let color;
+
+                    match road.road_type.as_str() {
+                        "asphalt" => color = [0.0, 0.0, 0.0, 1.0],
+                        "dirt" => color = [0.5, 0.5, 0.5, 1.0],
+                        "gravel" => color = [0.8, 0.8, 0.8, 1.0],
+                        _ => color = [0.0, 0.0, 0.0, 1.0],
+                    }
+
+                    line(color, 1.0, road.get_points(), scale, gl);
                 }
                 for intersection in &intersections {
                     ellipse(
-                        [0.0, 0.0, 0.0, 1.0],
+                        [0.0, 0.0, 1.0, 1.0],
                         [intersection.lon - 2.0, intersection.lat - 2.0, 4.0, 4.0],
                         scale.trans(intersection.lon, -intersection.lat),
                         gl,
