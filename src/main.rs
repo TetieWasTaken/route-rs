fd09f5ba1bc4fa313638207d38699dd8f018b983
TetieWasTaken <tjvssr@gmail.com>
2023-03-27 16:21:14 +0200

    feat: add wrong dir panic

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use std::env::current_dir;
 use std::error::Error;
 
 mod loaddata;
@@ -5,6 +6,10 @@ mod managers;
 mod window;
 
 fn main() -> Result<(), Box<dyn Error>> {
+    if !current_dir()?.to_str().unwrap().ends_with("src") {
+        panic!("[WRONG_DIR] Please run this program from the src directory");
+    }
+
     let (roads, intersections) = loaddata::load_data()?;
     window::init(roads, intersections);
     Ok(())
